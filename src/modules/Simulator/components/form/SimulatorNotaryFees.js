a502f1d6b6ba51b6df2c19f705f3a74262e01701
offaxis <clement@offaxis.io>
2020-09-02 11:31:06 +0200

    Simulator: Fix notary fees

--- src/modules/Simulator/components/form/SimulatorNotaryFees.js
+++ src/modules/Simulator/components/form/SimulatorNotaryFees.js
@@ -65,7 +65,8 @@ export default class SimulatorNotaryFees extends Component {
         let fee = null;
         this.state.tenantFees.forEach((tenantFee, index) => {
             if(!fee) {
-                if(price >= parseFloat(tenantFee.max_euro) || (index + 1) === this.state.tenantFees.length) {
+                if(price <= parseFloat(tenantFee.max_euro) || (index + 1) === this.state.tenantFees.length) {
+                    // console.log('Tenant fee found', price, tenantFee.max_euro, parseFloat(tenantFee.max_euro), price >= parseFloat(tenantFee.max_euro), (index + 1) === this.state.tenantFees.length);
                     fee = tenantFee;
                 }
             }
@@ -104,19 +105,30 @@ export default class SimulatorNotaryFees extends Component {
             const { montant, pourcentage } = this.getTenantFee(price);
             console.log('Tenand fee fetched', { montant, pourcentage });
 
-            let fee = (((price * parseFloat(pourcentage)) / 100) + parseFloat(montant) + 850) * 1.2;
-
-            // F = ((((P*Te)/100)+p+850)*1.2);
+            let fee = (
+                (
+                    (price * parseFloat(pourcentage))
+                    / 100
+                ) + parseFloat(montant) + 850
+            ) * 1.2;
+            // F = (
+            //     (
+            //         (P*Te)
+            //         / 100
+            //     ) + p + 850
+            // ) * 1.2;
 
             if(isNew) {
                 fee += (price * 0.715) / 100;
+                //     F = F+((P*0.715)/100);
             } else {
                 fee += (price * 5.8067) / 100;
+                //     F = F+((P*5.8067)/100);
             }
 
             fee += ((price * 0.1) / 100) + 50;
+            // F = F+((P*0.10)/100)+50;
 
-            // fee -= price;
 
             this.setState({
                 fee: `${Math.floor((fee) * 100) / 100}`.replace('.', ','),
